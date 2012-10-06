7986513af884676213e0cdd8dbda832df480c0f9
A. Akbar Hidayat <keripix@gmail.com>
2012-10-06 09:25:15 +0700

    generateMaze

--- src/bledos.js
+++ src/bledos.js
@@ -1,27 +1,59 @@
 window.onload = function () {
-  Crafty.init(500, 400);
-  //Crafty.canvas.init();
+  Crafty.init(480, 480);
+  Crafty.canvas.init();
+  
+  Crafty.sprite(16, 'resources/sprites/bomber.png', {
+    hWall: [10, 4],
+    vWall: [10, 5],
+    floor: [0, 0]
+  });
   
   // buat maze secara random
-  function generateMaze(params) {
-    
-    // jalankan callback
-    params.onComplete && params.onComplete();
+  function generateMaze() {
+    // gambar tembok
+    for (var i = 0; i < 15; i++) {
+      for (var j = 0; j < 15; j++) {
+        if (j === 0 || j === 14) {
+          // buat tembok mendatar
+          Crafty.e("2D, Canvas, hWall, solid")
+          .attr({x: i * 32, y: j * 32, h: 32, w: 32});
+        } else if (i === 0 || i === 14) {
+          // buat tembok tegak
+          Crafty.e("2D, Canvas, vWall, solid")
+          .attr({x: i * 32, y: j * 32, h: 32, w: 32});
+        } else {
+          // buat lantai
+          Crafty.e("2D, Canvas, floor")
+          .attr({x: i * 32, y: j * 32, h: 32, w: 32});
+          
+          // buat tembok di dalam arena
+          // kita ingin agar ada banyak tembok di dalam arena,
+          // jadi peluang adanya tembok adalah 2/5
+          if (Crafty.math.randomInt(1, 5) < 3) {
+            Crafty.e("2D, Canvas, hWall")
+            .attr({x: i * 32, y: j * 32, h: 32, w: 32});
+          }
+        }
+      }
+    }
   }
   
   // setting up stage
-  Crafty.scene('settingup', function () {
+  Crafty.scene('setup', function () {
     
-    generateMaze({
-      onComplete: function () {
-        // ketika generateMaze sudah selesai, maka kita jalankan scene 'main'
-        Crafty.scene('main');
-      }
+    Crafty.load(["resources/sprites/bomber.png"], function () {
+      Crafty.scene('main');
     });
   });
 
   // mari kita mulai  
   Crafty.scene('main', function () {
+    generateMaze();
+    
+    // hancurkan tembok di pojok pemain
     
   });
+  
+  // Jalankan dari awal
+  Crafty.scene('setup');
 }
