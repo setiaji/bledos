d0735f1681dbe722ce36320646114685fe4f3292
A. Akbar Hidayat <keripix@gmail.com>
2012-10-10 09:10:13 +0700

    orang sudah bisa bergerak

--- src/bledos.js
+++ src/bledos.js
@@ -5,7 +5,9 @@ window.onload = function () {
   Crafty.sprite(16, 'resources/sprites/bomber.png', {
     hWall: [10, 4],
     vWall: [10, 5],
-    floor: [0, 0]
+    iWall: [10, 4],
+    floor: [0, 0],
+    wong: [1, 4]
   });
   
   // buat maze secara random
@@ -28,9 +30,9 @@ window.onload = function () {
           
           // buat tembok di dalam arena
           // kita ingin agar ada banyak tembok di dalam arena,
-          // jadi peluang adanya tembok adalah 2/5
-          if (Crafty.math.randomInt(1, 5) < 3) {
-            Crafty.e("2D, Canvas, hWall")
+          // jadi peluang adanya tembok adalah 2/3
+          if (Crafty.math.randomInt(1, 3) < 3) {
+            Crafty.e("2D, Canvas, iWall, solid")
             .attr({x: i * 32, y: j * 32, h: 32, w: 32});
           }
         }
@@ -51,7 +53,104 @@ window.onload = function () {
     generateMaze();
     
     // hancurkan tembok di pojok pemain
+    // pos posisi dari pojok-pojok ini
+    var pos = [
+        {i: 1, j: 1},
+        {i: 2, j: 2},
+        {i: 1, j: 2},
+        {i: 12, j: 1},
+        {i: 13, j: 1},
+        {i: 13, j: 2},
+        {i: 1, j: 12},
+        {i: 1, j: 13},
+        {i: 2, j: 13},
+        {i: 12, j: 13},
+        {i: 13, j: 12},
+        {i: 13, j: 13}
+      ],
+      ent, idx, obj, player;
     
+    // iterasi untuk mencari apakah pada titik-titik pojok
+    // terdapat sebuah entitas iWall
+    for (idx = 0; idx < pos.length; idx += 1) {
+      obj = pos[idx];
+      ent = Crafty.map.search({_x: obj.i * 32, _y: obj.j * 32, _w: 32, _h: 32});
+      
+      // kita mengetahui bahwa entitas iWall berada di atas
+      // entitas floor.
+      if (ent[1]) {
+        if (ent[1]['__c'].iWall) {
+          Crafty.map.remove(ent[1]);
+        }
+      }
+    }
+    
+    Crafty.c('Wong', {
+      init: function () {
+        this.requires('SpriteAnimation, Collision, Wong')
+        .animate('walkRight', 4, 4, 7)
+        .animate('walkLeft', 4, 4, 7)
+        .animate('walkUp', 8, 4, 9)
+        .animate('walkDown', 1, 4, 3)
+        .bind('NewDirection', function (direction) {
+          if (direction.x < 0) {
+            if (!this.isPlaying('walkLeft')) {
+              this.stop().animate('walkLeft', 10, -1);
+            }
+          }
+          
+          if (direction.x > 0) {
+            if (!this.isPlaying('walkRight')) {
+              this.stop().animate('walkRight', 10, -1);
+            }
+          }
+          
+          if (direction.y < 0) {
+            if (!this.isPlaying('walkUp')) {
+              this.stop().animate('walkUp', 10, -1);
+            }
+          }
+          
+          if (direction.y > 0) {
+            if (!this.isPlaying('walkDown')) {
+              this.stop().animate('walkDown', 10, -1);
+            }
+          }
+          
+          if (!direction.x && !direction.y) {
+            this.stop();
+          }
+        })
+        .bind('Moved', function (from) {
+          // kalau ketemu dengan entitas 'solid', maka jangan sampai
+          // pemain menembusnya
+          if (this.hit('solid')) {
+            this.attr({x: from.x, y: from.y});
+          }
+        });
+      }
+    });
+    
+    Crafty.c('RightControls', {
+      init: function () {
+        this.requires('Multiway');
+      },
+      
+      rightControls: function (speed) {
+        this.multiway(speed, {
+          UP_ARROW: -90,
+          DOWN_ARROW: 90,
+          RIGHT_ARROW: 0,
+          LEFT_ARROW: 180
+        });
+        
+        return this;
+      }
+    });
+    
+    player = Crafty.e('2D, Canvas, wong, Wong, RightControls, Animate, Collision')
+    .attr({x: 32, y: 32, z: 1, w: 32, h: 32})
+    .rightControls(1);
   });
   
   // Jalankan dari awal
---
+++ src/wong.js
@@ -0,0 +1,30 @@
+// TODO gunakan RequireJs untuk menerapkan AMD
+var Wong = (function () {
+  Crafty.c('Wong', {
+    init: function () {
+      
+    }
+  });
+  
+  /*
+   TODO menerapkan Strategy Pattern
+   @link http://robdodson.me/blog/2012/08/03/javascript-design-patterns-strategy/
+  Crafty.c('Bedes1', {
+    init: function () {
+      
+    }
+  });
+  
+  Crafty.c('Bedes2', {
+    init: function () {
+      
+    }
+  });
+  
+  Crafty.c('Bedes3', {
+    init: function () {
+      
+    }
+  });
+  */
+})();
